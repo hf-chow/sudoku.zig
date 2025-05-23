94b80de7d9676f4caf89b73b70ad64d240f17fa0
Hon Chow <chow.hon.fung@chowsangsang.com>
2025-05-23 11:36:25 +0800

    adding getBoxNumber and refactoring

--- src/main.zig
+++ src/main.zig
@@ -1,1 +1,31 @@
-const sudoku = @import("sudoku");
+const std = @import("std");
+const sud = @import("sudoku.zig");
+const ParsingError = error{
+    MalformedBoard,
+    NotInteger,
+    NotWithinRange,
+};
+
+fn getBoxNumber(i: usize) u8 {
+    const row = i / 9;
+    const col = i % 9;
+    const box_num = (row / 3) * 3 + (col / 3);
+    return @intCast(box_num);
+}
+
+//fn parse(input: []const u8) !Board {
+//    if (input.len != 81) {
+//        return error.MalformedBoard;
+//    }
+//    for (input, 0..) |cell, index| {
+//
+//    }
+//}
+
+pub fn main() void {
+    const input = "085923476942576138763418592259841763678395241314267859896154327437682915521739684";
+    for (0.., input) |i, _| {
+        const boxNumber = sud.getBoxNumber(i);
+        std.debug.print("{}\n", .{boxNumber});
+    }
+}
--- src/sudoku.zig
+++ src/sudoku.zig
@@ -1,57 +1,24 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const ArrayList = std.ArrayList;
-const HashMap = std.StringHashMap;
+const Board = [9][9]u8;
 
-const rows = "ABCDEFGHI";
-const cols = "123456789";
-const cells = generate_cells();
-const unit_list = generate_unit_list();
-const units = generate_units();
-const peers = generate_peers();
+const ParsingError = error{
+    MalformedBoard,
+    NotInteger,
+    NotWithinRange,
+};
 
-fn generate_cells() [81][]const u8 {
-    var result: [81][]const u8 = undefined;
-    var idx: usize = 0;
-    for (rows) |r| {
-        for (cols) |c| {
-            result[idx] = &[_]u8{r, c};
-            idx += 1;
-        }
-    }
-    return result;
+pub fn getBoxNumber(i: usize) u8 {
+    const row = i / 9;
+    const col = i % 9;
+    const box_num = (row / 3) * 3 + (col / 3);
+    return @intCast(box_num);
 }
 
-fn generate_unit_list() [27][9][]const u8 {
-    var result: [27][9][]const u8 {
-        var idx: usize = 0;
-    }
-    const row_groups = [_][]const u8{ "ABC", "DEF", "GHI" };
-    const col_groups = [_][]const u8{ "123", "456", "789" };
-    for (rows_groups) |rg| {
-        for (col_groups) |cg| {
-            var box_idx: usize = 0;
-            for (rg) |r| {
-                for (cg) |c| {
-                    result[idx][box_idx] = &[_]u8{ r, c};
-                    box_idx += 1;
-                }
-            }
-            idx += 1;
-        }
-    }
-    for (cols) |c| {
-        for (rows, 0..) |r, i| {
-            result[idx][i] = &[_]u8{ r, c };
-        }
-        idx += 1;
-    }
-
-    for (rows) |r| {
-        for (cols, 0..) |c, i| {
-            result[idx][i] = &[_]u8{ r, c };
-        }
-        idx += 1;
-    }
-    return result;
-}
+//fn parse(input: []const u8) !Board {
+//    if (input.len != 81) {
+//        return error.MalformedBoard;
+//    }
+//    for (input, 0..) |cell, index| {
+//
+//    }
+//}
